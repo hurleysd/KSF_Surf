8c990cb1c8c3026525a6c7b6d6667bd74a228fcd
hurleysd <SeanDHurley@live.com>
2020-02-14 22:04:16 -0500

    Added twitch stream list functionality on the "Live" tab (using a temporary query)

--- KSF_Surf/ViewModels/LiveViewModel.cs
+++ KSF_Surf/ViewModels/LiveViewModel.cs
@@ -2,17 +2,68 @@
 using System.Windows.Input;
 using Xamarin.Essentials;
 using Xamarin.Forms;
+using RestSharp;
+using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace KSF_Surf.ViewModels
 {
     public class LiveViewModel : BaseViewModel
     {
+
+        internal RootObject streams;
+
         public LiveViewModel()
         {
             Title = "Live";
-            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://xamarin.com"));
+            twitchConnect();
         }
 
-        public ICommand OpenWebCommand { get; }
+        private void twitchConnect()
+        {
+            string clientID = ""; // HIDE!!!!
+
+            var client = new RestClient();
+            client.BaseUrl = new Uri("https://api.twitch.tv/helix/streams?first=6"); // TODO: change to actual surf streams (probably though a following list?)
+
+            var request = new RestRequest();
+            request.Method = Method.GET;
+            request.AddHeader("Client-ID", clientID);
+            request.RequestFormat = DataFormat.Json;
+
+            IRestResponse response = client.Execute(request);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                streams = JsonConvert.DeserializeObject<RootObject>(response.Content);
+            } 
+        }
+
+        // Classes for Twitch API response JSON deserialization
+        public class Datum
+        {
+            public string id { get; set; }
+            public string user_id { get; set; }
+            public string user_name { get; set; }
+            public string game_id { get; set; }
+            public string type { get; set; }
+            public string title { get; set; }
+            public int viewer_count { get; set; }
+            public DateTime started_at { get; set; }
+            public string language { get; set; }
+            public string thumbnail_url { get; set; }
+            public List<string> tag_ids { get; set; }
+        }
+
+        public class Pagination
+        {
+            public string cursor { get; set; }
+        }
+
+        public class RootObject
+        {
+            public List<Datum> data { get; set; }
+            public Pagination pagination { get; set; }
+        }
     }
 }
--- KSF_Surf/Views/LivePage.xaml.cs
+++ KSF_Surf/Views/LivePage.xaml.cs
@@ -11,31 +11,35 @@ using Xamarin.Forms.Xaml;
 using KSF_Surf.Models;
 using KSF_Surf.Views;
 using KSF_Surf.ViewModels;
+using System.Collections.ObjectModel;
 
 namespace KSF_Surf.Views
 {
     [DesignTimeVisible(false)]
     public partial class LivePage : ContentPage
     {
-        public LivePage() 
+        private LiveViewModel liveModel;
+
+        public LivePage()
         {
+            liveModel = new LiveViewModel();
+
             InitializeComponent();
             LayoutDesign();
         }
 
         private void LayoutDesign()
         {
-            
-
-            String cssServers = " Surf\t\t\t\t\t\t" + "map1" + "\n Europe\t\t\t\t\t" + "map2" + "\n Australia\t\t\t\t\t" + "map3" + "\n EUTop500\t\t\t\t" + "map4"
+            string cssServers = " Surf\t\t\t\t\t\t" + "map1" + "\n Europe\t\t\t\t\t" + "map2" + "\n Australia\t\t\t\t\t" + "map3" + "\n EUTop500\t\t\t\t" + "map4"
                                 + "\n Expert\t\t\t\t\t" + "map5" + "\n Veteran\t\t\t\t\t" + "map6" + "\n 100T US\t\t\t\t\t" + "map7" + "\n 100T EU\t\t\t\t\t" + "map8";
 
-            String csgoServers = " EasySurf\t\t\t\t\t" + "map1" + "\n AllSurf\t\t\t\t\t" + "map2" + "\n ExpertSurf\t\t\t\t" + "map3" + "\n Europe\t\t\t\t\t" + "map4";
+            string csgoServers = " EasySurf\t\t\t\t\t" + "map1" + "\n AllSurf\t\t\t\t\t" + "map2" + "\n ExpertSurf\t\t\t\t" + "map3" + "\n Europe\t\t\t\t\t" + "map4";
 
-            var servers = new List<String> { cssServers, csgoServers };
+            var servers = new List<string> { cssServers, csgoServers };
 
             ServersCarousel.ItemsSource = servers;
-            
+
+            LoadStreams();
         }
 
         private void ServersCarousel_CurrentItemChanged(object sender, CurrentItemChangedEventArgs e)
@@ -62,9 +66,36 @@ namespace KSF_Surf.Views
             ServersCarousel.Position = 1;
         }
 
-        private async void StartLoadingMaps()
+        ObservableCollection<LiveViewModel.Datum> streamData;
+        private ObservableCollection<LiveViewModel.Datum> Streams { get { return streamData; } }
+
+        private void LoadStreams()
         {
-            //ClientWebSocket ws = new ClientWebSocket();
+            try
+            {
+                streamData = new ObservableCollection<LiveViewModel.Datum>(liveModel.streams.data);
+            }
+            catch (NullReferenceException nullref)
+            {
+                // no handling (no streams online or Twitch query failed)
+                streamData = new ObservableCollection<LiveViewModel.Datum>() { new LiveViewModel.Datum { user_name = "(No streams online)" } };
+            }
+            finally
+            {
+                foreach (LiveViewModel.Datum datum in streamData) // applying image heights to stream thumbnails
+                {
+                    if (datum.thumbnail_url != null)
+                    {
+                        datum.thumbnail_url = datum.thumbnail_url.Replace("{height}", "75");
+                        datum.thumbnail_url = datum.thumbnail_url.Replace("{width}", "75");
+                    }
+                }
+                StreamsList.ItemsSource = streamData;
+                StreamsList.HeightRequest = (StreamsList.RowHeight * streamData.Count) + 1; // adding 1 disables scrolling within this ListView
+            }
         }
+
+        
     }
+
 }
