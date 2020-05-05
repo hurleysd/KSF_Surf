04bcb198f835b62ea3e0f20193745fe55c0fe978
Sean Hurley <44444572+hurleysd@users.noreply.github.com>
2020-05-05 18:00:02 -0400

    Delete Precondition.cs

--- KSF_Surf/ViewModels/Precondition.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace KSF_Surf.ViewModels
-{
-    class Precondition
-    {
-        internal readonly static string KSF = "RUFtZHFZNHM0ZUdLTTlOMldjRjlVZVk4cnltNnhz";
-        internal readonly static string STEAM = "ODRDMjBGRjI3MzhBMzQ2NEJDQjdCNzgwM0Y4NTRDMDI=";
-        internal readonly static string TWITCH = "aXo0eTNtd2dqZWRmZnYxdDJvaWZ3dXZuNW42aXly";
-    }
-}
