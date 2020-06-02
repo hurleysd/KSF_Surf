dd569f9167d1918a1ea3e28ebafc2ea09e9d29d2
hurleysd <hurleysd@dukes.jmu.edu>
2020-06-02 16:30:39 -0400

    android splash screens, updated icons

--- KSF_Surf.Android/MainActivity.cs
+++ KSF_Surf.Android/MainActivity.cs
@@ -7,11 +7,13 @@ using Xamarin.Forms;
 
 namespace KSF_Surf.Droid
 {
-    [Activity(Label = "KSF Surf", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "KSF Surf", Icon = "@mipmap/icon", Theme = "@style/Theme.Splash", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
+            SetTheme(Resource.Style.MainTheme); // change theme from splash screen
+
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
